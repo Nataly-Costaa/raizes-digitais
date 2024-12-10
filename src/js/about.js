fb1472145d2ea9d7f99a90b1202cee851a388551
Kaue Calixto <40962168+kauecalixto@users.noreply.github.com>
2024-12-10 17:07:04 -0300

    📲 Navbar menu

--- src/js/about.js
+++ src/js/about.js
@@ -1,5 +1,8 @@
-    function toggleMenu() {
-        const navbar = document.querySelector('.navbar');
-        navbar.classList.toggle('active');
-    }
+// Function to toggle the navbar menu
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+  }
+  
+
 
